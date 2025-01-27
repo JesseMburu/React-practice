0c3f68b9abad50661b45ad061c29885d30095a85
JesseMburu <jesse.mburu@strathmore.edu>
2025-01-27 14:27:01 +0300

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ const App = () => {
   return (
     <div>
       <Navbar />
+
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <Hero />
       </div>
+      
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
+import ReactPlayer from "react-player";
 import video1 from "../assets/video1.mp4"
 import video2 from "../assets/video2.mp4"
 
 const Hero = () => {
   return (
-      <div className="flex flex-column items-center mt-6 lg:mt-20">
+      <div className="flex flex-col items-center mt-6 lg:mt-20">
         <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
           VirtualR build tools 
           <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
@@ -11,6 +12,40 @@ const Hero = () => {
             for developers
             </span>
         </h1>
+        <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+          Empower your creativity and bring your 
+          VR app ideas to life with our intuitive development tools. 
+          Get started today and turn your imagination into immersive reality!
+          </p>
+
+          <div className="flex justify-center my-10">
+            <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md">
+              Get started
+            </a>
+            <a href="#" className=" py-3 px-4 mx-3 rounded-md border">
+              Documentation
+            </a>
+          </div>
+
+          <div className="flex flex-mt-10 justify-center">
+            <div className="mx-2 my-4 border border-orange-700 shadow-orange-400 rounded-lg w-1/2">
+              <ReactPlayer
+                url={video1}
+                playing
+                loop
+                muted
+              />
+            </div>
+            <div className="mx-2 my-4 border border-orange-700 shadow-orange-400 rounded-lg w-1/2">
+              <ReactPlayer
+                url={video2}
+                playing
+                loop
+                muted
+              />
+            </div>
+          </div>
+
       </div>
   )
 }
