34a0419fc53f1861c470d467950aedebba035065
JesseMburu <jesse.mburu@strathmore.edu>
2025-01-27 12:26:30 +0300

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar.jsx"
+import Hero from "./components/Hero.jsx"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Hero />
+      </div>
+    </div>
+  )
+}
+
+export default App
